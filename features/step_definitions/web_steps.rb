6945390a6dd6570039279922823e6c52e281e8d2
Hidenari Nozaki <hidenari@gmail.com>
2011-11-16 07:50:06 +0900

    Added title functionality for show page

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -22,3 +22,6 @@ Then /^I should see "([^"]*)"$/ do |arg1|
   page.has_content?(arg1).should be_true
 end
 
+Then /^I should be on the project page for "([^"]*)"$/ do |arg1|
+  current_path.should == project_path(Project.find_by_name!(arg1))
+end
