df8661b174fce5c1b48db9d308e1a12ac9346c73
Hidenari Nozaki <hidenari@gmail.com>
2011-11-15 22:10:09 +0900

    Creation of projects feature complete

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,24 @@
+require 'capybara/rails'
+
+Given /^I am on (.+)$/ do |page_name|
+  if page_name == 'the homepage'
+    visit(root_path)
+  end
+end
+
+When /^I follow "([^"]*)"$/ do |arg1|
+  click_link(arg1)
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
+  fill_in(arg1, {:with => arg2})
+end
+
+When /^I press "([^"]*)"$/ do |arg1|
+  click_button(arg1)
+end
+
+Then /^I should see "([^"]*)"$/ do |arg1|
+  page.has_content?(arg1).should be_true
+end
+
