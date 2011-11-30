ea4d4bc76dad982d514efab6b3de47099652839a
Hidenari Nozaki <hidenari@gmail.com>
2011-11-30 09:21:44 +0900

    Fix signing up feature, take user to alternative confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Ticketee::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
+  get '/awaiting_confirmation', :to => "users#confirmation", 
+    :as => 'confirm_user'
 
   resources :projects do
     resources :tickets
