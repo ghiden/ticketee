20612ee9c776a334055b7a4b63872b1779fc80a2
Hidenari Nozaki <hidenari@gmail.com>
2011-12-08 18:45:12 +0900

    Users can now leave comments on tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,4 +8,6 @@ class Ticket < ActiveRecord::Base
 
   has_many :assets
   accepts_nested_attributes_for :assets
+
+  has_many :comments
 end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -10,6 +10,10 @@ When /^I follow "([^"]*)"$/ do |arg1|
   click_link(arg1)
 end
 
+Given /^I follow "([^"]*)" within "([^"]*)"$/ do |link, parent|
+  page.find(parent).click_link(link)
+end
+
 When /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
   fill_in(arg1, {:with => arg2})
 end
@@ -48,3 +52,6 @@ When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
   attach_file(field, File.expand_path(path))
 end
 
+Then /^show me the page$/ do
+  save_and_open_page
+end
