78397c17133bbd3cfc00d1e088f343648232d893
Hidenari Nozaki <hidenari@gmail.com>
2012-01-17 22:43:46 +0900

    Add Receiver class to receive emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -5,6 +5,7 @@ class Notifier < ActionMailer::Base
     @comment = comment
     @user = user
     mail(:to => user.email,
+         :from => "Ticketee <ghiden+#{comment.project.id}+#{comment.ticket_id}@gmail.com>",
          :subject => 
     "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
   end
