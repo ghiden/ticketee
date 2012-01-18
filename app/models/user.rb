819bc728604e9f9005f412af2ce2606d3c2d7d19
Hidenari Nozaki <hidenari@gmail.com>
2012-01-18 18:39:19 +0900

    Implemented token-based authentication API base

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :permissions
+
+  before_save :ensure_authentication_token
 
   def to_s
     "#{email} (#{admin? ? "Admin" : "User" })"
